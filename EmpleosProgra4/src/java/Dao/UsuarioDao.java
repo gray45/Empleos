6d49d3262d0a661f597153d89d814cefbffc6fce
gray45 <gravenrrh123@gmail.com>
2018-03-20 10:10:59 -0600

    Merge origin/master
    Conflicts:
    	TransPorTico/src/java/cr/ac/una/prograiv/project/test/TestChofer.java

---
+++ EmpleosProgra4/src/java/Dao/UsuarioDao.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import domain.Usuario;
+import java.util.List;
+import org.hibernate.HibernateException;
+import utils.HibernateUtil;
+
+/**
+ *
+ * @author grave
+ */
+public class UsuarioDao extends HibernateUtil implements IBaseDao<Usuario, Integer> {
+
+    @Override
+    public void save(Usuario obj) {
+        try {
+            iniciarOperacion();
+            getSesion().save(obj);
+            getTransac().commit();
+        } catch (HibernateException he) {
+            manejarException(he);
+            throw he;
+        } finally {
+            getSesion().close();
+        }
+
+    }
+
+    @Override
+    public void merge(Usuario obj) {
+        try {
+            iniciarOperacion();
+            getSesion().merge(obj);
+            getTransac().commit();
+        } catch (HibernateException he) {
+            manejarException(he);
+            throw he;
+        } finally {
+            getSesion().close();
+        }
+    }
+
+    @Override
+    public Usuario findByID(Integer key) {
+        Usuario chofer = null;
+
+        try {
+            iniciarOperacion();
+            chofer = (Usuario) getSesion().get(Usuario.class, key);
+        } finally {
+            getSesion().close();
+        }
+        return chofer;
+    }
+
+    @Override
+    public void delete(Usuario obj) {
+        try {
+            iniciarOperacion();
+            getSesion().delete(obj);
+            getTransac().commit();
+        } catch (HibernateException he) {
+            manejarException(he);
+            throw he;
+        } finally {
+            getSesion().close();
+        }
+
+    }
+
+    @Override
+    public List<Usuario> findAll() {
+        try {
+            List<Usuario> choferes;
+            iniciarOperacion();
+            choferes = (List<Usuario>) getSesion().createQuery("FROM Usuario").list();
+            return choferes;
+        } catch (HibernateException he) {
+            manejarException(he);
+            throw he;
+        } finally {
+            getSesion().close();
+        }
+    }
+
+    @Override
+    public List findByQuery(String query) {
+        try {
+            List<Usuario> choferes;
+            iniciarOperacion();
+            choferes =  getSesion().createQuery(query).list();
+            return choferes;
+        } catch (HibernateException he) {
+            manejarException(he);
+            throw he;
+        } finally {
+            getSesion().close();
+        }
+    }
+
+    @Override
+    public List findHQLQuery(String query) {
+        List lista=null;
+        try{
+            iniciarOperacion();
+            lista = getSesion().createQuery(query).list();
+            return lista;
+        }catch(HibernateException he){
+            manejarException(he);
+            throw he;
+        }finally{
+            getSesion().close();
+        }
+        
+    }
+}
